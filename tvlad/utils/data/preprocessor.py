93564eadd149b259be94d1cf067b8f906f41d167
cvprw-22 <102473023+cvprw-22@users.noreply.github.com>
2022-03-27 22:18:14 +0800

    Add files via upload

---
+++ tvlad/utils/data/preprocessor.py
@@ -0,0 +1,43 @@
+from __future__ import absolute_import
+import torch
+from torch.utils.data import DataLoader, Dataset
+import os
+import re
+import os.path as osp
+import numpy as np
+import random
+import math
+from PIL import Image
+
+
+class Preprocessor(Dataset):
+    def __init__(self, dataset, root=None, transform=None):
+        super(Preprocessor, self).__init__()
+        self.dataset = dataset
+        self.root = root
+        self.transform = transform
+
+
+    def __len__(self):
+        return len(self.dataset)
+
+
+    def __getitem__(self, indices):
+        if isinstance(indices, (tuple, list)):
+            return [self._get_single_item(index) for index in indices]
+        else:
+            return self._get_single_item(indices)
+
+
+    def _get_single_item(self, index):
+        fname, pid, x, y = self.dataset[index]
+        fpath = fname
+        if self.root is not None:
+            fpath = osp.join(self.root, fname)
+
+        img = Image.open(fpath).convert('RGB')
+
+        if (self.transform is not None):
+            img = self.transform(img)
+
+        return img, fname, pid, x, y
