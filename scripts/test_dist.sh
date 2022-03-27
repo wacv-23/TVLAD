19cf2265303c2235f0fa229d696a52dc7b709dfd
cvprw-22 <102473023+cvprw-22@users.noreply.github.com>
2022-03-27 22:15:03 +0800

    Add files via upload

---
+++ scripts/test_dist.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+PYTHON=${PYTHON:-"python"}
+GPUS=4
+
+# RESUME=$1
+ARCH=${1-mobilenetv3_large}
+
+DATASET=${2-pitts}
+SCALE=${3-30k}
+
+if [ $# -lt 1 ]
+  then
+    echo "Arguments error: <MODEL PATH>"
+    echo "Optional arguments: <ARCH (default:vgg16)> <DATASET (default:pitts)> <SCALE (default:250k)>"
+    exit 1
+fi
+
+while true # find unused tcp port
+do
+    PORT=$(( ((RANDOM<<15)|RANDOM) % 49152 + 10000 ))
+    status="$(nc -z 127.0.0.1 $PORT < /dev/null &>/dev/null; echo $?)"
+    if [ "${status}" != "0" ]; then
+        break;
+    fi
+done
+
+$PYTHON -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT --use_env \
+test.py --launcher pytorch \
+    -d ${DATASET} --scale ${SCALE} -a ${ARCH} \
+    --test-batch-size 4 -j 2 \
+    --width 640 --height 480 --features 4096 \
+    --vlad --reduction \
+    --resume logs/netVLAD/pitts30k-mobilenetv3_large/model_best.pth.tar
+    # --sync-gather
